27bd25a94cde4bccc81135ba6f08c5f6d7e4db0a
Walid BOUSSOU <121355412+walidbosso@users.noreply.github.com>
2024-01-22 19:21:49 +0100

    login and generate token

--- springboot-backend/src/main/java/univ/iwa/controller/UserController.java
+++ springboot-backend/src/main/java/univ/iwa/controller/UserController.java
@@ -1,9 +1,14 @@
 package univ.iwa.controller;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,28 +23,38 @@ import univ.iwa.service.UserInfoService;
 @RestController
 @RequestMapping("/auth") 
 public class UserController { 
+	
     @Autowired UserInfoService service; 
     @Autowired JwtService jwtService; 
     @Autowired AuthenticationManager authenticationManager; 
+    
     @GetMapping("/welcome") 
     public String welcome() {return "Welcome this endpoint is not secure";} 
+    
     @PostMapping("/addNewUser") 
     public String addNewUser(@RequestBody UserInfo userInfo) { 
         return service.addUser(userInfo); 
     } 
-    @GetMapping("/user/userProfile") 
-    @PreAuthorize("hasAuthority('ROLE_USER')") 
-    public String userProfile() { return "Welcome to User Profile"; } 
+    
+    @GetMapping("/user/assistantProfile") 
+    @PreAuthorize("hasAuthority('ROLE_ASSISTANT')") 
+    public String userProfile() { return "Welcome to Assistant Profile"; } 
+  
     @GetMapping("/admin/adminProfile") 
     @PreAuthorize("hasAuthority('ROLE_ADMIN')") 
     public String adminProfile() { return "Welcome to Admin Profile"; } 
-    @PostMapping("/generateToken") 
-    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) { 
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())); 
-        if (authentication.isAuthenticated()) { 
-            return jwtService.generateToken(authRequest.getUsername()); 
-        } else { 
-            throw new UsernameNotFoundException("invalid user request !"); 
-        } 
-    } 
+  
+    @PostMapping("/generateToken")
+    public ResponseEntity<String> authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            List<String> roles = authentication.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toList());
+            String token = jwtService.generateToken(authRequest.getUsername(), roles.get(0));
+            return ResponseEntity.ok("{\"Token\":\"" + token + "\",\"role\":\"" + roles.get(0) + "\"}");
+        } else {
+            throw new UsernameNotFoundException("Invalid user request!");
+        }
+    }
 } 
--- springboot-backend/src/main/java/univ/iwa/service/JwtService.java
+++ springboot-backend/src/main/java/univ/iwa/service/JwtService.java
@@ -17,8 +17,9 @@ import java.util.function.Function;
 public class JwtService { 
 
 	public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437"; 
-	public String generateToken(String userName) { 
+	public String generateToken(String userName,String role) { 
 		Map<String, Object> claims = new HashMap<>(); 
+		claims.put("role",role);
 		return createToken(claims, userName); 
 	} 
 	private String createToken(Map<String, Object> claims, String userName) { 
