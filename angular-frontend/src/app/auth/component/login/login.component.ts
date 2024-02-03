647a44c4ee0db527bdbac6aee42932fa6f9a2c96
MedAymane1 <aymaneassladday@gmail.com>
2024-02-04 00:57:56 +0100

    some routing modifications | login(not termined)

--- angular-frontend/src/app/auth/component/login/login.component.ts
+++ angular-frontend/src/app/auth/component/login/login.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'app/model/user.model';
+import { UserAuthsService } from 'app/services/user-auths.service';
+import { UserService } from 'app/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  // Utilisez le modèle User ici
+  user: User = {
+    username: '',
+    password: '',
+  };
 
+  constructor(
+    private userService: UserService,
+    private userAuthsService: UserAuthsService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  public login() {
+    console.log('Inside Login Function');
+
+    this.userService.generateToken(this.user).subscribe(
+      (response: any) => {
+        console.log('role: ' + response.role);
+        console.log('accessToken: ' + response.accessToken);
+        this.userAuthsService.setRole(response.role);
+        this.userAuthsService.setToken(response.accessToken);
+        console.log('getRole: ' + this.userAuthsService.getRole());
+        console.log('getToken: ' + this.userAuthsService.getToken());
+
+        // if (response.role === 'ROLE_ADMIN') {
+        //   this.router.navigate(['/admin']);
+        // } else if (response.role === 'ROLE_ASSISTANT') {
+        //   this.router.navigate(['/assistant']);
+        // } else if (response.role === 'ROLE_FORMAT') {
+        //   this.router.navigate(['/format']);
+        // }
+      },
+      (error) => {
+        console.log('In error');
+        console.log(error);
+      }
+    );
+  }
 }
